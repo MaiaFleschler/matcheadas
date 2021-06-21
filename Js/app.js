b654a61c7cbe3d4076fb16c2ae507100bff9019d
Maia <maiafleschler@gmail.com>
2021-06-21 17:51:46 -0300

    Primer parte estilos

--- Js/app.js
+++ Js/app.js
@@ -1,4 +1,12 @@
 'use strict';
+const container = document.getElementById("container");
+const titulo = document.createElement("h1");
+const textoTitulo = document.createTextNode("MatcheADAs");
+titulo.appendChild(textoTitulo);
+titulo.setAttribute("class", "estiloLetras");
+titulo.style.fontSize = "45px";
+container.appendChild(titulo);
+
 
 const botonFacil = document.createElement("button");
 const botonNormal = document.createElement("button");
@@ -6,7 +14,14 @@ const botonDificil = document.createElement("button");
 const botonFacilTexto = document.createTextNode("Facil");
 const botonNormalTexto = document.createTextNode("Normal");
 const botonDificilTexto = document.createTextNode("Dificil");
-const grilla = document.getElementById("grilla");
+
+const contenedorGrilla = document.createElement("div");
+contenedorGrilla.setAttribute("class", "contenedorGrilla");
+const grilla = document.createElement("div");
+grilla.setAttribute("class", "grilla");
+contenedorGrilla.appendChild(grilla);
+document.body.appendChild(contenedorGrilla);
+
 
 botonFacil.appendChild(botonFacilTexto);
 botonNormal.appendChild(botonNormalTexto);
@@ -26,13 +41,12 @@ const crearCeldas = (cantidad,tamanio) =>{
         arrayMatriz.push([]);
         for(let j = 0; j < col; j++) {
             const celda = document.createElement('div');
-            celda.style.fontSize = `${cellSize-20}px`;
             celda.style.width = `${cellSize}px`;
             celda.style.height = `${cellSize}px`;
             celda.style.position = 'absolute';
             celda.style.left = `${i*cellSize}px`;
             celda.style.top = `${j*cellSize}px`;
-            celda.style.border = '1px solid #000';
+            celda.style.fontSize = `${cellSize-20}px`;
             const emoji = emojis[Math.floor(Math.random()*emojis.length)];
             arrayMatriz[i].push(emoji);
             celda.innerText = emoji;
@@ -50,4 +64,3 @@ botonNormal.addEventListener("click", ()=>{
 botonDificil.addEventListener("click", ()=>{
 	crearCeldas(7,72);
 });
-
